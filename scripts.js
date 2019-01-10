e58d118a4a17ed5ab5372c5bd903f99905c43875
JulioPitta <34603654+JulioPitta@users.noreply.github.com>
2019-01-10 20:43:31 -0200

    Coloquei animações

---
+++ scripts.js
@@ -0,0 +1,3 @@
+var name = prompt("Qual é seu nome? (Letra maiúscula em início de nome!)");
+      var namechip = document.getElementById("welcome");
+      namechip.innerText = "Bem-vindo à minha página " + name + "!";
